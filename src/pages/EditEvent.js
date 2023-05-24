994809978c036c2a797022252ddb9a2329ee4b0a
Suwandi Kurniawan <suwandikurniawan89@gmail.com>
2023-05-24 14:03:36 +0700

    Learn redux done

---
+++ src/pages/EditEvent.js
@@ -0,0 +1,12 @@
+import { useRouteLoaderData } from 'react-router-dom';
+import EventForm from '../components/EventForm';
+
+const EditEventPage = () => {
+    const data = useRouteLoaderData('event-detail');
+
+    const event = data.event;
+
+    return <EventForm method="PATCH" event={event}/>;
+}
+
+export default EditEventPage;
